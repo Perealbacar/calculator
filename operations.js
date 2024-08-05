62120b70cadfdfbc1ecbb21a2657b43bddab6ec0
Perealbacar <37050221+Perealbacar@users.noreply.github.com>
2024-08-05 12:01:54 +0200

    Calculations are working

--- operations.js
+++ operations.js
@@ -1,40 +1,144 @@
 let totalNum = 0;
 
-function add(lastNum){
+const operators = "+-*/%";
+const numbers = "1234567890";
+
+let operator = undefined;
+let lastNum = undefined;
+
+
+function Add(lastNum){
     return totalNum + lastNum
 }
 
-function subtract(lastNum){
+function Subtract(lastNum){
     return totalNum - lastNum
 }
 
-function multiply(lastNum){
+function Multiply(lastNum){
     return totalNum * lastNum
 }
 
-function divide(lastNum){
+function Divide(lastNum){
     return totalNum / lastNum
 }
 
-function operate(firstNum, operator, lastNum){
+function Operate(firstNum, operator, lastNum){
+    let result = 0;
+    firstNum = parseFloat(firstNum);
+    lastNum = parseFloat(lastNum);
+    totalNum = parseFloat(totalNum);
+    
     switch (operator){
         case "+":
-            totalNum = firstNum.add(lastNum);
+            result = Add(lastNum);
+            totalNum = result;            
             break;
         case "-": 
-            totalNum = firstNum.subtract(lastNum);
+            totalNum = Subtract(lastNum);
             break;
         case "*": 
-            totalNum = firstNum.subtract(lastNum);
+            totalNum = Multiply(lastNum);
             break;
         case "/":
-            totalNum = firstNum.subtract(lastNum);
+            totalNum = Divide(lastNum);
             break;
         case "":
             totalNum = 0;
             break;        
     }
-
+    ResetValues();
     return totalNum;
 }
+
+
+
+function ResetValues(){
+
+    operator = undefined;
+    lastNum = undefined;
+}
+
+
+const display = document.querySelector(".display");
+const calculator = document.querySelector(".calculator");
+const equal = document.querySelector(".equal");
+
+function ChangeDisplay(text){
+    display.textContent = text;
+}
+
+function IsOperator(text){
+
+    let result = text.split('').filter((char) => operators.includes(char));
+
+    console.log("result = " + result);
+
+    let bool = result.length > 0;
+
+    if(bool){
+        operator = result[0];
+    }
+
+    console.log(bool);
+    return bool;
+    
+}
+
+equal.addEventListener("click", e => {
+
+    lastNum = display.textContent.split('').filter((char) => numbers.includes(char)).join('');
+    console.log("totalNum: " + totalNum + "operator:" + operator + "lastNum:" + lastNum + "equals")
+    
+    console.log(operator);
+    console.log( operator === "+");
+    console.log(totalNum);
+    console.log(lastNum);
+
+    display.textContent = Operate(totalNum, operator, lastNum);
+    e.stopPropagation()
+
+})
+
+
+calculator.addEventListener("click", e =>{
+    
+    let text = e.target.textContent;
+    if (totalNum === 0){
+        
+        display.textContent = text;
+        totalNum = text;
+        console.log("1")
+    }
+
+    else if(!IsOperator(text)){
+
+        if(operator != undefined && lastNum != undefined){
+            
+            
+            ChangeDisplay(text);
+        }
+        else{
+
+            display.textContent += text;
+        }
+        
+        console.log("2")
+    }   
+    
+    else{
+        ChangeDisplay(text);
+        console.log("3")
+    }
+    
+})
+    
+
+
+
+
+
+
+
+
 
